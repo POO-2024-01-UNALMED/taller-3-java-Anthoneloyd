4815059cf2c87df25c3fb78149421c6f8bce97f8
andres <agarcesb@unal.edu.co>
2024-02-23 13:59:18 -0500

    buenas tardes

---
+++ src/main/java/taller3/televisores/TV.java
@@ -0,0 +1,14 @@
+package taller3.televisores;
+
+public class TV {
+    private Marca marca;
+    private int canal;
+    private int precio;
+    private boolean estado;
+    private int volumen;
+    private Control control;
+
+    public TV(Marca marca, boolean estado){
+
+    }
+}
